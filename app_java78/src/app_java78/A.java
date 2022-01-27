67d636f654d7ae47e5d74da131628d05230b8196
Sniper-Codes <98414272+Sniper-Codes@users.noreply.github.com>
2022-01-28 04:25:27 +0530

    First commit

---
+++ app_java78/src/app_java78/A.java
@@ -0,0 +1,11 @@
+package app_java78;
+
+public class A {
+public static void main(String[] args) {
+	try {
+		Fil f = new File("D://")
+	} catch (Exception e) {
+		// TODO: handle exception
+	}
+}
+}

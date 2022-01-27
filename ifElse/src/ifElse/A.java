d9c83a54482b6d7651f9f6a2fff6259b88dc6421
Sniper-Codes <98414272+Sniper-Codes@users.noreply.github.com>
2022-01-28 04:33:25 +0530

    First commit

---
+++ ifElse/src/ifElse/A.java
@@ -0,0 +1,20 @@
+package ifElse;
+
+public class A {
+	public static void main(String[] args) {
+		int x = 30;
+		int y = 40;
+		if (x!=y) {
+			System.out.println("Not equals: ");
+		} else {
+			System.out.println("Equals");
+	}
+			
+		}
+		
+	}
+
+
+
+
+

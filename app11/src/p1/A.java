253f193c83b233daa3afbf2dde06041c50f809d3
Sniper-Codes <98414272+Sniper-Codes@users.noreply.github.com>
2022-01-28 04:26:32 +0530

    First commit

---
+++ app11/src/p1/A.java
@@ -0,0 +1,15 @@
+package p1;
+
+public class A {
+	public static void main(String[] args) {
+		
+	
+	int x =22;
+	int y = 42;
+	int z=(x+y);
+	
+	System.out.println(z);
+
+	}
+
+}
---
+++ app11/src/p1/B.java
@@ -0,0 +1,15 @@
+package p1;
+
+public class B extends A {
+	
+	float f = z;
+public static void main(String[] args) {
+	B b1 = new B ();
+	
+	b1.test();
+	
+}
+public void test() {
+	System.out.println(f);
+}
+}

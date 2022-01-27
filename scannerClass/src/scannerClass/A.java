bfd7d6b134629a44842f5e89c037fac493304644
Sniper-Codes <98414272+Sniper-Codes@users.noreply.github.com>
2022-01-28 04:41:52 +0530

    First commit

---
+++ scannerClass/src/scannerClass/A.java
@@ -0,0 +1,9 @@
+package scannerClass;
+
+import java.util.Scanner;
+
+public class A {
+	public void test() {
+	Scanner  scan = new Scanner(System.in);
+	}
+}
---
+++ scannerClass/src/scannerClass/B.java
@@ -0,0 +1,12 @@
+package scannerClass;
+
+public class B extends A {
+	public static void main(String[] args) {
+		System.out.println("ENter your name :");
+		String id = "A";
+		System.out.println("Your name is :" +id );
+		
+		
+	}
+
+}

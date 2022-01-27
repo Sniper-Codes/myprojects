c576b120effd17632d78e50184a397c070153bbd
Sniper-Codes <98414272+Sniper-Codes@users.noreply.github.com>
2022-01-28 04:36:00 +0530

    First commit

---
+++ InterfaceOverriding/src/B.java
@@ -0,0 +1,5 @@
+
+public interface B extends A {
+public void test2();
+
+}
---
+++ InterfaceOverriding/src/C.java
@@ -0,0 +1,27 @@
+
+public class C implements B{
+
+	@Override
+	public void test1() {
+		System.out.println("Hey");// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void test2() {
+		System.out.println("superMan");// TODO Auto-generated method stub
+		
+	}
+	public static void main(String[] args) {
+		C c1 = new C();
+		c1.test1();
+		c1.test2();
+
+	}
+
+
+	
+
+}
+
+ 

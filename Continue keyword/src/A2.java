0e9e73691ed4ddc5c872e27edf9a0af396f64a83
Sniper-Codes <98414272+Sniper-Codes@users.noreply.github.com>
2022-01-28 04:29:42 +0530

    First commit

---
+++ Continue keyword/src/A2.java
@@ -0,0 +1,16 @@
+
+public class A2 {
+	public static void main(String[] args) {
+		int x = 30;
+		int y = 40;
+		if (x!=y) {
+			System.out.println("Not equals: ");
+		} else {
+			System.out.println("Equals"); //;("Equals");
+	}
+			
+		}
+		
+	}
+
+
